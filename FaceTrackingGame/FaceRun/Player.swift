0039c3e896b68dba12eee351a3af62986e9b4dd9
brunavcosta <62903611+brunavcosta@users.noreply.github.com>
2021-09-16 09:37:17 -0300

    Add files via upload

---
+++ FaceTrackingGame/FaceRun/Player.swift
@@ -0,0 +1,23 @@
+//
+//  Player.swift
+//  FaceRun
+//
+//  Created by Brian Advent on 21.06.18.
+//  Copyright © 2018 Brian Advent. All rights reserved.
+//
+
+import SpriteKit
+
+enum PlayerState:String {
+    case neutral = "Neutral"
+    case up = "Up"
+    case down = "Down"
+    case left = "Left"
+    case right = "Right"
+    
+}
+
+class Player: SKSpriteNode {
+
+    
+}
